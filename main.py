debfb832b690ea7bdd69ab64afb16340853bb65c
Krinal09 <krinalgami@gmail.com>
2025-01-04 04:53:36 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.Wine_Shop_Project import logger
 from src.Wine_Shop_Project.pipeline.stage_01_data_ingetion import DataIngestionTrainingPapeline
 from src.Wine_Shop_Project.pipeline.stage_02_data_validation import DataValidationTrainingPapeline
+from src.Wine_Shop_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPapeline
+from src.Wine_Shop_Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPapeline
+
 
 STAGE_NAME = "Data Ingetion stage"
 
@@ -21,6 +24,32 @@ try:
     logger.info(f"---- stage {STAGE_NAME} started ----")
     data_validation =  DataValidationTrainingPapeline()    
     data_validation.main()
+    logger.info(f"---- stage {STAGE_NAME} completed ---- \n\nx=======x")
+        
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f"---- stage {STAGE_NAME} started ----")
+    data_transformation =  DataTransformationTrainingPapeline()    
+    data_transformation.main()
+    logger.info(f"---- stage {STAGE_NAME} completed ---- \n\nx=======x")
+        
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training stage"
+
+try:
+    logger.info(f"---- stage {STAGE_NAME} started ----")
+    obj =  ModelTrainerTrainingPapeline()    
+    obj.main()
     logger.info(f"---- stage {STAGE_NAME} completed ---- \n\nx=======x")
         
 except Exception as e:
--- src/Wine_Shop_Project/pipeline/stage_04_model_trainer.py
+++ src/Wine_Shop_Project/pipeline/stage_04_model_trainer.py
@@ -1,24 +1,25 @@
 from src.Wine_Shop_Project.config.configuration import ConfigurationManager
-from src.Wine_Shop_Project.components.data_validation import DataValidation
+from src.Wine_Shop_Project.components.model_trainer import ModelTrainer
 from src.Wine_Shop_Project import logger
+from pathlib import Path
 
-STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Model Training stage"
 
-class DataValidationTrainingPapeline:
+class ModelTrainerTrainingPapeline:
     def __init__(self):
         pass
     
     def main(self):
+        
         config = ConfigurationManager()
-        data_validation_config = config.get_data_validation_config()
-        data_validation = DataValidation(config=data_validation_config)
-        data_validation.validate_all_columns()
-            
-            
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+        
 if __name__ == '__main__':
     try:
         logger.info(f"---- stage {STAGE_NAME} started ----")
-        obj =  DataValidationTrainingPapeline()    
+        obj =  ModelTrainerTrainingPapeline()    
         obj.main()
         logger.info(f"---- stage {STAGE_NAME} completed ---- \n\nx=======x")
         
